a0a35d9e2de00618c94fe7cb2cad723998c0b2d8
Lakshitha Geethmal <dgtlgeethmal@students.nsbm.ac.lk>
2023-03-12 16:18:39 +0530

    complete dashboard

--- EMS - SCNE/Dashboard.cs
+++ EMS - SCNE/Dashboard.cs
@@ -1,14 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Reflection.Emit;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using ComponentFactory.Krypton.Toolkit;
 
 namespace EMS___SCNE
 {
